80230b209dafafd6944c047ec45480d4444f941e
Sanjy Kanagarasa <sanjy.kanagarasa@outlook.com>
2020-09-17 15:16:18 +0200

    Layout changes

--- Controller/UploadController.cs
+++ Controller/UploadController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EelamHeroes.Controller
@@ -36,6 +39,31 @@ namespace EelamHeroes.Controller
             catch (Exception ex)
             {
                 return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPost("[action]")]
+        [Route("api/Image/Save")]
+        public void Save(IList<IFormFile> UploadFiles)
+        {
+            try
+            {
+                foreach (var file in UploadFiles)
+                {
+                    {
+                        var randomString = RandomString(20);
+                        var folderPath = $"images/post/{DateTime.Now.Year}";
+                        var imageFileName = UploadFileToUrl(file, folderPath, randomString + ".jpg");
+                        Response.Headers.Add("name", imageFileName);
+                        Response.StatusCode = 200;
+                        Response.ContentType = "application/json; charset=utf-8";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
             }
         }
         [HttpPost("upload/hero/{id}")]
